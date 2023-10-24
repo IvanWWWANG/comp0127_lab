f049dac398feb87af494caad5ba74e265c708be9
Solène Dietsch <solene.dietsch@gmail.com>
2023-10-24 14:10:54 +0100

    Update or lab03_example_srv to match the recording

--- lab03/lab03_example_srv/src/client.py
+++ lab03/lab03_example_srv/src/client.py
@@ -5,13 +5,12 @@ import numpy as np
 import random
 import time
 
+#TODO: import the SRV file from its corresponding folder, as well as its Request
 from lab03_example_srv.srv import point_rot, point_rotRequest
 
 def point_rotation_client():
-
     rospy.wait_for_service('rotate_pt')
     client = rospy.ServiceProxy('rotate_pt', point_rot)
-
     request = point_rotRequest()
 
     while not rospy.is_shutdown():
@@ -19,6 +18,9 @@ def point_rotation_client():
         request.p.x = random.uniform(-2.0, 2.0)
         request.p.y = random.uniform(-2.0, 2.0)
         request.p.z = random.uniform(-2.0, 2.0)
+
+        print(request)
+
 
         quaternion = np.random.rand(4)
         quaternion /= np.linalg.norm(quaternion)
@@ -28,17 +30,13 @@ def point_rotation_client():
         request.q.z = quaternion[2]
         request.q.w = quaternion[3]
 
-        res = client(request)
-        print(res)
+        response = client(request)
+        print(response)
+
         time.sleep(3)
-
-
-
 
 if __name__ == '__main__':
     try:
         point_rotation_client()
     except rospy.ROSInterruptException:
         pass
-
- 
